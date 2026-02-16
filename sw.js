d84ac95ec28f771f5a76df96055089b1ec8c10ee
danton2405-sudo <danton2405@gmail.com>
2026-02-16 21:54:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-// sw.js - minimal version
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
-});
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
